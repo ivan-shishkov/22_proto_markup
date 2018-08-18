54f6af2d442e313aa5dbf7cf2f57d7c15518e126
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-08-18 15:02:51 +0300

    Added script to render site from templates

---
+++ build.py
@@ -0,0 +1,20 @@
+from staticjinja import make_site
+
+context = {
+    'number_requisitions_start_page': 4,
+    'number_requisitions_per_page': 8,
+    'number_suppliers_info_per_page': 6,
+    'number_reviews': 2,
+    'username': 'Леонид Федорович',
+}
+
+if __name__ == '__main__':
+    site = make_site(
+        searchpath='templates',
+        outpath='rendered_site',
+        staticpaths=['static'],
+        contexts=[
+            ('.*.html', context),
+        ],
+    )
+    site.render()
